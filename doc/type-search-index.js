a58437746d9ced972d9094b2264a34f69e014725
Pablo Alvarez <pabloalv567@gmail.com>
2022-11-09 20:32:35 -0600

    Javadoc

---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"CancionJJ"},{"p":"<Unnamed>","l":"ICancion"},{"p":"<Unnamed>","l":"IIpod_simulator"},{"p":"<Unnamed>","l":"IPodJJ"},{"p":"<Unnamed>","l":"Main"}];updateSearchResults();
